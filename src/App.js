9c991ebdf7433964dbf799f81383e65fe16157c0
chinomak01 <1HYaguana.a@gmail.com>
2024-08-16 08:10:20 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import Sidebar from './components/Sidebar';
+import Contenido from './components/Contenido';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="d-flex" style={{ display: 'flex' }}>
+        <Sidebar />
+        <Contenido />
+      </div>
     </div>
   );
 }
